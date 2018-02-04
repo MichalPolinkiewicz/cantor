0d102ba0006abf8ee10089cdaf9c7c19a5e92952
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2018-02-04 20:06:33 +0100

    commit

---
+++ src/main/java/com/exchange/controller/Asd.java
@@ -0,0 +1,7 @@
+package com.exchange.controller;
+
+/**
+ * Created by Lenovo on 04.02.2018.
+ */
+public class Asd {
+}
--- src/main/java/com/exchange/controller/CantorController.java
+++ src/main/java/com/exchange/controller/CantorController.java
@@ -37,5 +37,4 @@ public class CantorController {
         return cantorFacade.getActualizationTime();
     }
 
-
 }
