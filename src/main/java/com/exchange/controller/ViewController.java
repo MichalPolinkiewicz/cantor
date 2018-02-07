efbd92479eec2b247c9eb67baf9c806fe73177d8
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2018-02-07 23:52:08 +0100

    commit

--- src/main/java/com/exchange/controller/ViewController.java
+++ src/main/java/com/exchange/controller/ViewController.java
@@ -1,8 +1,15 @@
 package com.exchange.controller;
 
+import com.exchange.domain.dto.CurrencyDto;
+import com.exchange.facade.CantorFacade;
+import com.exchange.mapper.CurrencyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Lenovo on 07.02.2018.
@@ -10,6 +17,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/v1/exchange")
 public class ViewController {
+
+    @Autowired
+    private CantorFacade cantorFacade;
+    @Autowired
+    private CurrencyMapper currencyMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "/welcome")
     public String getWelcome(){
@@ -22,7 +34,9 @@ public class ViewController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/logged")
-    public String getLogged(){
+    public String getLogged(Map <String, Object> model){
+        List<CurrencyDto> currencyDtos = currencyMapper.mapToCurrencyDtoList(cantorFacade.getCantor());
+        model.put("currencies", currencyDtos);
         return "logged";
     }
 }
